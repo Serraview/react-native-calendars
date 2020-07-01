8c70068fd44bbae4e89627c99a99748bd24b64b9
Danil <inpendio@gmail.com>
2020-07-01 18:34:01 -0400

    getDate workaround

--- src/interface.js
+++ src/interface.js
@@ -16,7 +16,11 @@ function xdateToData(xdate) {
     day: xdate.getDate(),
     timestamp: XDate(dateString, true).getTime(),
     dateString: dateString,
-    dateObject:xdate.toDate()
+    dateObject: new Date(
+      xdate.getFullYear(),
+      xdate.getMonth(),
+      xdate.getDate(),
+    ),
   };
 }
 
@@ -24,23 +28,32 @@ function parseDate(d) {
   // console.log('interface/parseDate',d);
   if (!d) {
     return;
-  } else if (d.timestamp) { // conventional data timestamp
+  } else if (d.timestamp) {
+    // conventional data timestamp
     return XDate(d.timestamp, true);
-  } else if (d instanceof XDate) { // xdate
+  } else if (d instanceof XDate) {
+    // xdate
     return XDate(d.toString('yyyy-MM-dd'), true);
-  } else if (d.getTime) { // javascript date
-    const dateString = d.getFullYear() + '-' + padNumber((d.getMonth() + 1)) + '-' + padNumber(d.getDate());
+  } else if (d.getTime) {
+    // javascript date
+    const dateString =
+      d.getFullYear() +
+      '-' +
+      padNumber(d.getMonth() + 1) +
+      '-' +
+      padNumber(d.getDate());
     return XDate(dateString, true);
   } else if (d.year) {
-    const dateString = d.year + '-' + padNumber(d.month) + '-' + padNumber(d.day);
+    const dateString =
+      d.year + '-' + padNumber(d.month) + '-' + padNumber(d.day);
     return XDate(dateString, true);
-  } else if (d) { // timestamp number or date formatted as string
+  } else if (d) {
+    // timestamp number or date formatted as string
     return XDate(d, true);
   }
 }
 
 module.exports = {
   xdateToData,
-  parseDate
+  parseDate,
 };
-
